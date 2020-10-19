735942539e45c2cf04bb4a3c74533405730bb4db
Mike Miller <github@mikeage.net>
2020-10-19 13:06:27 +0300

    Add stream action

--- reddit_chat_archiver/reddit_chat_archiver.py
+++ reddit_chat_archiver/reddit_chat_archiver.py
@@ -18,14 +18,15 @@ except ImportError:
 
     class Fore(object):
         pass
-    Fore.RESET = Fore.RED = ""
+    Fore.RESET = Fore.RED = Fore.BLUE = Fore.GREEN = ""
 
 
 HOST = "sendbirdproxy.chat.redditmedia.com"
+AI = '2515BDA8-9D3A-47CF-9325-330BC37ADA13'  # This is reddit's chat AI.
 LOGGER = logging.getLogger(__name__)
 
 
-def dump_session_key(username, password, twofa):
+def do_songbird_login(username, password, twofa):
     headers = {'User-Agent': 'Firefox'}  # It seems to fail with no User-Agent.
     data = {
         'op': 'login',
@@ -44,8 +45,49 @@ def dump_session_key(username, password, twofa):
     response = requests.get('https://s.reddit.com/api/v1/sendbird/me', headers=headers)
     sb_access_token = response.json()['sb_access_token']
     LOGGER.info("sb_access_token -> %s", sb_access_token)
-    ai = '2515BDA8-9D3A-47CF-9325-330BC37ADA13'  # This is reddit's chat AI.
-    ws = websocket.create_connection(f"wss://sendbirdproxy.chat.redditmedia.com/?p=_&pv=29&sv=3.0.82&ai={ai}&user_id={user_id}&access_token={sb_access_token}")
+    return reddit_session, sendbird_scoped_token, user_id, sb_access_token
+
+
+class Chat(object):
+    def __init__(self, url):
+        self.ws = websocket.WebSocketApp(url, on_message=lambda ws, msg: self.on_message(msg), on_error=lambda ws, error: self.on_error(error), on_close=lambda ws: self.on_close())
+        self._last_error = None
+
+    @staticmethod
+    def on_message(msg):
+        msg_type = msg[0:4]
+        if msg_type == "LOGI":
+            print(Style.RESET_ALL + Fore.GREEN + "Logged in!" + Style.RESET_ALL)
+        if msg_type == "MESG":
+            print(msg)
+            print(Style.RESET_ALL + Fore.RED + msg["user"]["name"] + Fore.RESET + ": " + msg["message"])
+
+    @staticmethod
+    def on_close():
+        LOGGER.error("Closed!")
+
+    def on_error(self, error):
+        self._last_error = error
+        LOGGER.error("Error received! (%s)", str(error))
+
+    def start(self):
+        while True:
+            self.ws.run_forever(ping_interval=15, ping_timeout=5)
+            if self._last_error == websocket.WebSocketConnectionClosedException:
+                LOGGER.warning("Reconnecting...")
+                continue
+            return
+
+
+def stream(username, password, twofa):
+    _, _, user_id, sb_access_token = do_songbird_login(username, password, twofa)
+    ws = Chat(f"wss://sendbirdproxy.chat.redditmedia.com/?p=_&pv=29&sv=3.0.82&ai={AI}&user_id={user_id}&access_token={sb_access_token}")
+    ws.start()
+
+
+def dump_session_key(username, password, twofa):
+    _, _, user_id, sb_access_token = do_songbird_login(username, password, twofa)
+    ws = websocket.create_connection(f"wss://sendbirdproxy.chat.redditmedia.com/?p=_&pv=29&sv=3.0.82&ai={AI}&user_id={user_id}&access_token={sb_access_token}")
     result = ws.recv()
     ws.close()
     key = json.loads(result[result.find('{'):])['key']
@@ -112,7 +154,8 @@ def get_all_messages(key, channel_url, starting_timestamp=0):
             if message["type"] == "ADMM":
                 print("%s" % message["message"])
             elif message["type"] == "MESG":
-                print(Style.RESET_ALL + Fore.RED + message["user"]["nickname"] + Fore.RESET + ": " + message["message"])
+                j = json.loads(message[4:])
+                print(Style.RESET_ALL + Fore.RED + j["user"]["nickname"] + Fore.RESET + ": " + j["message"])
             else:
                 print("UKNOWN MESSAGE: %s" % message)
         starting_timestamp = messages[-1]["created_at"]
@@ -129,6 +172,10 @@ def main():
     subparsers = parser.add_subparsers(title="Operation", help='Command to run', dest='action')
     subparsers.required = True
 
+    parser_stream = subparsers.add_parser('stream', help='List all group channels and URLs')  # , parents=[common_parser])
+    parser_stream.add_argument('-u', '--username', help="Reddit Username", default=os.getenv("REDDIT_USERNAME", None))
+    parser_stream.add_argument('-p', '--password', help="Reddit Password", default=os.getenv("REDDIT_PASSWORD", None))
+    parser_stream.add_argument('-2', '--2fa', dest="twofa", help="Reddit 2FA code", default=os.getenv("REDDIT_2FA", None))
     parser_dump_session_key = subparsers.add_parser('dump-session-key', help='List all group channels and URLs')  # , parents=[common_parser])
     parser_dump_session_key.add_argument('-u', '--username', help="Reddit Username", default=os.getenv("REDDIT_USERNAME", None))
     parser_dump_session_key.add_argument('-p', '--password', help="Reddit Password", default=os.getenv("REDDIT_PASSWORD", None))
@@ -137,13 +184,16 @@ def main():
     parser_list_group_channels.add_argument('-k', '--key', help="Session-Key (get using Web Inspector from a browser)", default=os.getenv("REDDIT_SESSION_KEY", None))
     parser_get_group_channel = subparsers.add_parser('get-group-channel', help='Get all messages from the specified chat')  # , parents=[common_parser])
     parser_get_group_channel.add_argument("channel_url", help="Channel URL")
-    parser_get_group_channel.add_argument('-k', '--key', help="Session-Key (get using Web Inspector from a browser)", required=True)
+    parser_get_group_channel.add_argument('-k', '--key', help="Session-Key (get using Web Inspector from a browser)", default=os.getenv("REDDIT_SESSION_KEY", None))
 
     args = parser.parse_args()
 
     levels = [logging.WARNING, logging.INFO, logging.DEBUG]
     level = levels[min(len(levels) - 1, args.verbose)]
     logging.basicConfig(level=level)
+
+    websocket.enableTrace(level <= logging.DEBUG)
+
     logging.getLogger('prawcore').setLevel(logging.ERROR)
 
     if args.action == "list-group-channels":
@@ -155,6 +205,8 @@ def main():
     elif args.action == "dump-session-key":
         assert args.username and args.password
         dump_session_key(args.username, args.password, args.twofa)
+    elif args.action == "stream":
+        stream(args.username, args.password, args.twofa)
     LOGGER.info("Done")
 
 
